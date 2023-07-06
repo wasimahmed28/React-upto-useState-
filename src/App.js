b2237dbbfc87b0c585967fa9ae02445af1265890
Syed Wasim Ahmed <iamwasimreal28@gmail.com>
2023-07-06 20:40:33 +0530

    Create, Read, Update done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
+import apiRequest from './apiRequest';
 import { useState, useEffect } from 'react';
 function App() {
   const API_URL= ' http://localhost:3500/items';
@@ -33,16 +34,38 @@ function App() {
 
 
 
-  const addItem = (item) => {
+  const addItem  = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions ={ 
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions);
+    if(result) setFetchErr(result);
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions ={
+      method: "PATCH",
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON. stringify({checked:myItem[0].checked})
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result=await apiRequest(reqUrl, updateOptions);
+    if(result) setFetchErr(result)
   }
 
   const handleDelete = (id) => {
